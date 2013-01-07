eb648492df8da25887ffa342726fb2d95bf0e5ea
jaredwilli <jaredwilli@gmail.com>
2013-01-07 13:36:41 -0500

    added all files

---
+++ js/main.js
@@ -0,0 +1,119 @@
+bmi_SafeAddOnload = null;
+
+if (typeof note !== 'object') {
+	note = {};
+}
+
+note = {
+	common: {
+		init: function() {
+			note.init();
+		}
+	},
+	init: function() {
+				$(function() {
+			var onSampleResized = function(e) {
+				var columns = $(e.currentTarget).find('th, td');
+			};
+
+			$('#resizable').colResizable({
+				minWidth: 100,
+				liveDrag: true,
+				draggingClass: 'dragging',
+				onResize: onSampleResized
+			});
+		});
+
+		var td = document.getElementsByTagName('td');
+
+		console.log($('td:first').width());
+		console.log($('#note-nav').width());
+
+		$('#note-nav').find('a').on('click', function(e) {
+			e.preventDefault();
+			console.log($(this).parent());
+
+			if ($(this).parent().hasClass('expanded')) {
+				$(this).parent().removeClass('expanded');
+
+				$(this).next().addClass('hidden');
+			}
+			else {
+				$(this).parent().addClass('expanded');
+				$(this).next().removeClass('hidden');
+			}
+		});
+
+	},
+
+	utils: {
+		extend: function(d, e, c) {
+			var b = function() {}, a;
+			b.prototype = e.prototype;
+			d.prototype = new b();
+			d.prototype.constructor = d;
+			d.superclass = e.prototype;
+			if (e.prototype.constructor == Object.prototype.constructor) {
+				e.prototype.constructor = e;
+			}
+			if (c) {
+				for (a in c) {
+					if (c.hasOwnPropterty(a)) {
+						d.prototype[a] = c[a];
+					}
+				}
+			}
+		},
+		isArray: function(array) {
+			return (array.constructor.toString().indexOf('Array') !== -1);
+		},
+		randomKey: function(array) {
+			return array[Math.floor(Math.random() * array.length)];
+		},
+		keyExists: function(key, search) {
+			if (!search || (search.constructor !== Array && search.constructor !== Object)) { return false; }
+			for (var i = 0; i < search.length; i++) {
+				if (search[i] === key) { return true; }
+			}
+			return key in search;
+		},
+		createCache: function(requestFunction) {
+			var cache = {};
+			return function(key, callback) {
+				if (!cache[key]) {
+					cache[key] = $.Deferred(function(defer) {
+						requestFunction(defer, key);
+					}).promise();
+				}
+				return cache[key].done(callback);
+			};
+		}
+	}
+};
+
+UTIL = {
+	fire: function(func, funcname, args) {
+		var namespace = note;
+		funcname = (funcname === undefined) ? 'init' : funcname;
+		if (func !== '' && namespace[func] && typeof namespace[func][funcname] == 'function') {
+			namespace[func][funcname](args);
+		}
+	},
+	loadEvents: function() {
+		var b = document.body;
+		var bid = b.id;
+		//console.log(bid);
+		UTIL.fire('common');
+		UTIL.fire(bid);
+/*
+		var classes = b.clasaname.split(/\s+/), test = classes.length;
+		for (var i = 0; i < test; i++) {
+			UTIL.fire(classes[i]);
+			UTIL.fire(classes[i], bid);
+		};
+*/
+		UTIL.fire('common', 'finalize');
+	}
+};
+//kick it all off here
+$(document).ready(UTIL.loadEvents);
